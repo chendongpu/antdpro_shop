a01168dd17a39d9e3e54b23a69654c5ed075c20b
chendongpu <2323178881@qq.com>
2021-05-19 13:04:58 +0800

    封装网络请求

--- src/services/user.js
+++ src/services/user.js
@@ -3,7 +3,9 @@ export async function query() {
   return request('/api/users');
 }
 export async function queryCurrent() {
-  return request('/api/currentUser');
+  //return request('/api/currentUser');
+  return request.post('/auth/login');
+
 }
 export async function queryNotices() {
   return request('/api/notices');
