492fc2746a793d7799b1ec692620816003a24a8f
chendongpu <2323178881@qq.com>
2021-05-21 13:20:24 +0800

    add：完成添加用户表单

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,13 +1,43 @@
-import React, { useRef } from 'react';
+import React, { useRef,useState } from 'react';
 import { PlusOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Avatar, message, Menu, Switch } from 'antd';
+import { Button, Avatar, message, Menu, Switch,Modal } from 'antd';
 import ProTable  from '@ant-design/pro-table';
 import {PageContainer} from '@ant-design/pro-layout';
 import request from 'umi-request';
 import {getUsers,lockUser} from "../../services/user";
+import ProForm, { ProFormText } from '@ant-design/pro-form';
 
 export default () => {
     const actionRef = useRef();
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+    const handleForm =async (value) => {
+        const res=await add(value);
+
+        if(res.code===0){
+            props.dispatch({
+                type:'todo/getTodoList',
+                payload:null
+            });
+            message.success(res.message);
+        }else{
+            message.error("error");
+        }
+
+    };
+
 
     const columns = [
 
@@ -91,12 +121,19 @@ export default () => {
             dateFormatter="string"
             headerTitle="高级表格"
             toolBarRender={() => [
-                    <Button key="button" icon={<PlusOutlined />} type="primary">
+                    <Button key="button" icon={<PlusOutlined />} type="primary" onClick={showModal}>
                         新建
                     </Button>
                 ]
             }
             />
+            <Modal title="添加用户" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
+                <ProForm onFinish={(value)=>{handleForm(value)}}>
+                    <ProFormText name="name" label="昵称" placeholder="请输入昵称" rules={[{required:true,message:"请输入昵称"}]} />
+                    <ProFormText name="email" label="邮箱" placeholder="请输入邮箱" rules={[{required:true,message:"请输入邮箱"},{type:'email',message:"邮箱格式不正确"},]} />
+                    <ProFormText.Password name="password" label="密码" placeholder="请输入密码" rules={[{required:true,message:"请输入密码"},{min:6,message:"密码最小6位"},]} />
+                </ProForm>
+            </Modal>
         </PageContainer>
     );
 }
